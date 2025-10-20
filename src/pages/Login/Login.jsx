1e2b37853cd5d3c9517393c2e4cc8d01e5b78099
codeWithNithin <nithinkottary25@gmail.com>
2025-10-20 14:25:58 +0530

    login page with signin and sign up both

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div
       style={{
@@ -15,6 +22,46 @@ const Login = () => {
       }}
     >
       <Header />
+
+      <div className="form-container bg-black p-4">
+        <form
+          action=""
+          className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        >
+          <h1 className="font-bold text-3xl py-4">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignInForm && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-4 my-4 w-full bg-gray-700"
+            />
+          )}
+          <input
+            type="text"
+            name="email"
+            placeholder="Email or phone number"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+
+          <button className="px-4 py-2 bg-red-600 cursor-pointer">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+
+          <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign Up Now"
+              : "Already registered? Sign In Now."}
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
